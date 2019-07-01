53c9c8d48c733e9049841626dcb9741205f10a4c
jsumnerPhD <jonathonresumner@gmail.com>
2019-06-30 22:32:25 -0400

    Build virtualenvs and check along with requirements files

--- pre_commit_hooks/safety_check.py
+++ pre_commit_hooks/safety_check.py
@@ -1,16 +1,33 @@
 from __future__ import print_function
+import subprocess
 import sys
 from safety.cli import check
 
 
 def main(argv):
+    '''
+    - Pass files with 'requirement' in the filename as args to safety
+    - Build each file into virtualenv (to pin versions) and call safety on venv
+    '''
+
     try:
+        # Check *requirement* files
         check.main(['--full-report'] + sum((['-r', f] for f in argv), []))
-        return 0
     except SystemExit as error:
-        if error.code == 0:
-            return 0
-        return 1
+        if error.code != 0:
+            return 1
+
+    for file in argv:
+        try:
+            # Build virtual environments to force pinning of versions
+            subprocess.run(["pip", "install", "-r", str(file)])
+            check.main(['--full-report'])
+            subprocess.run(["pip", "uninstall", "-ry", str(file)])
+        except SystemExit as error:
+            if error.code == 0:
+                return 0
+                
+    return 1
 
 
 if __name__ == '__main__':
